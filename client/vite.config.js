0a928bc5101c92d1186b64efab86795406088856
ryanlui13 <ryan.luigi.2020@gmail.com>
2026-06-27 19:45:55 -0400

    any changes made

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [,
+  plugins: [
     react(),
     tailwindcss(),
   ],
-  base: '/CafePannaWebsite/',
+  base: '/', // This forces Vercel to look at the root domain
 })
